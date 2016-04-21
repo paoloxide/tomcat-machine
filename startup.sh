59b84c56cb20d5bbca37a392f17c741d2bb8e1f5
Lambojon <paolo.t.lambojon@accenture.com>
2016-04-21 19:51:42 +0800

    Reengineered the docker compose for Tomcat 7

---
+++ startup.sh
@@ -0,0 +1,123 @@
+#!/bin/bash -e
+
+OVERRIDES=
+
+
+echo ' 
+   ############ #######  ##       ##   ########    ###   ############     ##########
+        ##     ##     ## ## ## ## ## ##           ## ##       ##                 ##
+        ##     ##     ## ##  # #  ## ##          ##   ##      ##                ##
+        ##     ##     ## ##   #   ## ##         ##     ##     ##               ##
+        ##     ##     ## ##       ## ##         #########     ##               ##
+        ##     ##     ## ##       ## ##         ##     ##     ##               ##
+        ##      #######  ##       ##   ######## ##     ##     ##               ##
+'
+
+usage(){
+  echo "Usage: ./startup.sh -m <MACHINE_NAME> -a <AWS_ACCESS_KEY_ID>(optional) -s <AWS_SECRET_ACCESS_KEY>(optional) -c <VPC_ID> -r <AWS_DEFAULT_REGION>(optional) -v <VOLUME_DRIVER>(optional) -n <CUSTOM_NETWORK_NAME>(optional) -l <LOGGING_DRIVER>(optional) -f path/to/additional_override1.yml(optional) -f path/to/additional_override2.yml(optional) ..."
+}
+
+# Defaults
+export VOLUME_DRIVER=local
+export LOGGING_OVERRIDE=' -f etc/logging/syslog/default.yml'
+export CUSTOM_NETWORK_NAME=adopnetwork
+
+
+while getopts "m:n:a:s:c:r:f:v:l:" opt; do
+  case $opt in
+    m)
+      export MACHINE_NAME=${OPTARG}
+      ;;
+    n)
+      export CUSTOM_NETWORK_NAME=${OPTARG}
+      ;;
+    a)
+      export AWS_ACCESS_KEY_ID=${OPTARG}
+      ;;
+    s)
+      export AWS_SECRET_ACCESS_KEY=${OPTARG}
+      ;;
+    c)
+      export VPC_ID=${OPTARG}
+      ;;
+    r)
+      export AWS_DEFAULT_REGION=${OPTARG}
+      ;;
+    v)
+      export VOLUME_DRIVER=${OPTARG}
+      ;;
+    f)
+      export OVERRIDES="${OVERRIDES} -f ${OPTARG}"
+      ;;
+    l)
+      export LOGGING_OVERRIDE=" -f ${OPTARG}"
+      ;;
+    *)
+      echo "Invalid parameter(s) or option(s)."
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z $MACHINE_NAME ] | \
+	[ -z $CUSTOM_NETWORK_NAME ] | \
+	[ -z $VPC_ID ]; then
+  usage
+  exit 1
+fi
+
+if [ -z $AWS_ACCESS_KEY_ID ] & \
+    [ -f ~/.aws/credentials ];
+then
+  echo "Using default AWS credentials from ~/.aws/credentials"
+  eval $(grep -v '^\[' ~/.aws/credentials | sed 's/^\(.*\)\s=\s/export \U\1=/')
+fi
+
+if [ -z $AWS_DEFAULT_REGION ] & \
+    [ -f ~/.aws/config ];
+then
+  echo "Using default AWS region from ~/.aws/config"
+  eval $(grep -v '^\[' ~/.aws/config | sed 's/^\(region\)\s=\s/export AWS_DEFAULT_REGION=/')
+fi
+
+source env.config.sh
+
+# Create Docker machine if one doesn't already exist with the same name
+if $(docker-machine env $MACHINE_NAME > /dev/null 2>&1) ; then
+	echo "Docker machine '$MACHINE_NAME' already exists"
+else
+  if [ -z $AWS_ACCESS_KEY_ID ]; then
+    docker-machine create --driver amazonec2 --amazonec2-vpc-id $VPC_ID --amazonec2-instance-type t2.large --amazonec2-root-size 100 $MACHINE_NAME
+  else
+    docker-machine create --driver amazonec2 --amazonec2-access-key $AWS_ACCESS_KEY_ID --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY --amazonec2-vpc-id $VPC_ID --amazonec2-instance-type t2.large --amazonec2-region $AWS_DEFAULT_REGION --amazonec2-root-size 100 $MACHINE_NAME
+  fi
+fi
+
+# Create Docker network if one doesn't already exist with the same name
+eval "$(docker-machine env $MACHINE_NAME)"
+if ! docker network create $CUSTOM_NETWORK_NAME; then
+	echo "Docker network '$CUSTOM_NETWORK_NAME' already exists"
+fi
+
+# Run the Docker compose commands
+export TARGET_HOST=$(docker-machine ip $MACHINE_NAME)
+export LOGSTASH_HOST=$(docker-machine ip $MACHINE_NAME)
+docker-compose -f docker-compose.yml -f etc/volumes/${VOLUME_DRIVER}/default.yml $LOGGING_OVERRIDE ${OVERRIDES} pull
+docker-compose -f docker-compose.yml -f etc/volumes/${VOLUME_DRIVER}/default.yml $LOGGING_OVERRIDE ${OVERRIDES} up -d
+
+# Generate and copy the certificates to jenkins slave
+$(pwd)/generate_client_certs.sh ${DOCKER_CLIENT_CERT_PATH} >/dev/null 2>&1
+
+# Tell the user something useful
+echo
+echo '##########################################################'
+echo
+echo SUCCESS, your new ADOP instance is ready!
+echo
+echo Run these commands in your shell:
+echo '  eval $(docker-machine env $MACHINE_NAME)'
+echo '  source env.config.sh'
+echo
+echo Navigate to http://$TARGET_HOST in your browser to use your new DevOps Platform!
+
